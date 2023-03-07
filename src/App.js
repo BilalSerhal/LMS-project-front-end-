c8d109d878b110066bf506cb7da7da8a907690b1
Rasha <badranrasha685@gmail.com>
2023-03-07 13:30:08 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
-
+import { useState, useEffect } from 'react';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
+import './components/Login/Login'
+import Login from './components/Login/Login';
+import TeacherSuperAdmin from './components/Dashboard/TeacherSuperAdmin';
 function App() {
+ 
+    
   return (
-    <div >
-      Hello
+    <BrowserRouter>
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/TeacherSuperAdmin" element={<TeacherSuperAdmin/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
+  
 }
 
 export default App;
---
+++ src/components/Dashboard/TeacherSuperAdmin.jsx
@@ -0,0 +1,8 @@
+import './TeacherSuperAdmin.css';
+function TeacherSuperAdmin(){
+<dev>
+    Hiiiii
+</dev>
+}
+
+export default TeacherSuperAdmin;
