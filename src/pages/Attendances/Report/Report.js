542185eccdb6756d75f7bd5754e1e530dc8b353c
achrafAR <achraf.alrachini@gmail.com>
2023-03-09 11:55:23 +0200

    report Attendance By Grade And Section

--- src/pages/Attendances/Report/Report.js
+++ src/pages/Attendances/Report/Report.js
@@ -2,32 +2,45 @@ import React, { useState } from 'react'
 import './Report.css'
 
 function Report() {
+
+
+
 const attendance = [
     {
         id:1,
         name:"achraf",
         lastName:"AlRachini",
         status:'Present',
+        grade:'grade1',
+        section:'sectionB',
     },
     {
         id:1,
         name:"bilal",
         status:'Absent',
+        grade:'grade1',
+        section:'sectionB',
     },
     {
         id:1,
         name:"rasha",
         status:'Present',
+        grade:'grade1',
+        section:'sectionB',
     },
     {
         id:1,
         name:"ranim",
         status:'absent',
+        grade:'grade1',
+        section:'sectionB',
     },
 
 ]
 
-const [name, setName] = useState('')
+const [name, setName] = useState('');
+const [grade, setGrade] = useState('');
+const [section, setSection] = useState('');
 return (
     <div>
         <div className='search'>
@@ -35,14 +48,16 @@ return (
                 <input placeholder='Search By Name' onChange={event => setName(event.target.value)}/>
             </div>
             <div className='searchSection'>
-                <input placeholder='Search By Section'/>
+                <div><input placeholder='Search By Grade' onChange={event => setGrade(event.target.value)}/></div>
+                <div><input placeholder='Search By Section' onChange={event => setSection(event.target.value)}/></div>
             </div>
         </div>
 
+        <div className='report'>
         <div className='status'>
             {attendance.filter(Attendance => {
                 if(name === ''){
-                    return Attendance;
+                    return null;
                 }
                 else if (Attendance.name.toLowerCase().includes(name.toLowerCase())){
                     return Attendance.status;
@@ -57,6 +72,26 @@ return (
             </div>
             
             ))}
+        </div>
+        <div className='status'>
+            {attendance.filter(Attendance => {
+                if(grade === '' && section === ''){
+                    return null;
+                }
+                else if (Attendance.grade.toLowerCase()===(grade.toLowerCase()) && Attendance.section.toLowerCase()===(section.toLowerCase()) ){
+                    return Attendance.status;
+                }
+            })
+            .map((Attendance, index)=>(
+            <div className='statusByName'>
+                <div className='boxStatus1' key={index}>
+                    <h5>{Attendance.name} {Attendance.lastName}</h5>
+                    <h5>{Attendance.status}</h5>
+            </div>
+            </div>
+            
+            ))}
+        </div>
         </div>
     </div>
 )
