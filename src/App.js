fcea090626fe416e6e64ff6a27ea48f9e4ebd39e
Rasha <badranrasha685@gmail.com>
2023-03-16 13:17:52 +0200

    merge rasha to div

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import { Routes ,Route } from 'react-router-dom';
-// import Levels from '../src/pages/levels/levels';
-// import Sections from '../src/pages/sections/sections';
 import { useEffect, useState } from "react";
 import "./App.css";
-
-import Course from "./components/course";
-import { useState, useEffect } from 'react';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import './components/Login/Login';
 import Login from './components/Login/Login';
 import StudentSuperAdmin from './components/StudentSuperAdmin/StudentSuperAdmin';
 import TeacherSuperAdmin from './components//TeacherSuperAdmin/TeacherSuperAdmin';
-
+import Course from "./components/course";
 
 function App() {
-
-  const [menubar, setMenuBar] = useState(false);
-  
   return (
-    <div >
-      Hello
+    <BrowserRouter>
+    <div className="all">
+    <Router>
+      <Routes>
+      
+        <Route path="/Course" element={<Course/>}/>
+         <Route path="/" element={<Login />} />
+          <Route path="/StudentSuperAdmin" element={<StudentSuperAdmin />} />
+          <Route path="/TeacherSuperAdmin" element={<TeacherSuperAdmin />} />
+      </Routes>
+      
+    </Router>
     </div>
+    </BrowserRouter>
   );
-
-  }
- {/* <Route exact path="/" element={<MainPic />} />
-              <Route path="/levels" element={<Levels />} />
-              <Route path="/sections" element={<Sections />} />
-              <Route path="/teachers" element={<Teachers />} />
-              <Route path="/studants" element={<Students />} /> */}
+}
 
 export default App;
